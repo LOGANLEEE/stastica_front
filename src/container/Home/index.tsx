d994ac2377714accec94a76821b0c26b34f42fdd
LOGAN LEE <dlfjgkssk1@naver.com>
2020-07-03 23:39:47 +0100

    middle of design Home

--- src/container/Home/index.tsx
+++ src/container/Home/index.tsx
@@ -1,50 +1,76 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector, useDispatch } from 'react-redux';
-
 import { Grid } from '@material-ui/core';
-import Navigation from 'components/Navigation';
-import ThemeSwitcher from 'components/ThemeSwitcher';
 import styled from 'styled-components';
 
-// import { incrementAsync, selectHomeTest, MAIN_TEST } from './Slice';
 import { selectUi, THEME_HANDLER } from 'container/Ui/Slice';
 
-const Home = ({}) => {
+import { Navigation } from 'components/Navigation';
+import { ThemeSwitcher } from 'components/ThemeSwitcher';
+import { Card } from 'components/Card';
+import { Sign } from 'components/Sign';
+
+import { Wrapper } from './Wrapper';
+
+// import { incrementAsync, selectHomeTest, MAIN_TEST } from './Slice';
+
+export const Home = () => {
 	const Ui = useSelector(selectUi);
 	const { isDark, menus } = Ui;
 	// const dispatch = useDispatch();
 
 	return (
 		<Wrapper>
-			<Grid container direction='column'
-				justify='center'
-				alignItems='center'
-			>
+			<div className='head'>
 				<Grid
 					className='header'
 					container
 					direction='row'
-					justify='center'
-					alignItems='center'
+					justify='space-around'
+					alignItems='stretch'
 				>
-					<Navigation menus={menus} />
-					<ThemeSwitcher
-						isDark={isDark}
-						THEME_HANDLER={THEME_HANDLER}
-					/>
+					<Grid item>
+						<ThemeSwitcher
+							isDark={isDark}
+							THEME_HANDLER={THEME_HANDLER}
+						/>
+					</Grid>
+					<Grid item>
+						<Navigation menus={menus} />
+					</Grid>
+
+					<Grid item>
+						<Sign isDark={isDark} />
+					</Grid>
 				</Grid>
-				<div className='body'>body</div>
-				<div className='foot'>foot</div>
-			</Grid>
+			</div>
+			<div className='body'>
+				<Grid
+					container
+					direction='row'
+					justify='space-around'
+					alignItems='stretch'
+				>
+					<Grid xs={12} sm={6} item>
+						<Card isDark={isDark} />
+					</Grid>
+					<Grid xs={12} sm={6} item>
+						<Card isDark={isDark} />
+					</Grid>
+					<Grid xs={12} sm={6} item>
+						<Card isDark={isDark} />
+					</Grid>
+					<Grid xs={12} sm={6} item>
+						<Card isDark={isDark} />
+					</Grid>
+				</Grid>
+			</div>
+			<div className='foot'>foot</div>
 		</Wrapper>
 	);
 };
 
-const Wrapper = styled.div``;
-
 Home.propTypes = {};
 
 Home.defaultProps = {};
-
-export default Home;
