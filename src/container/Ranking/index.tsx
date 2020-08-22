4508d52409b7b241fbc4d76d198b28f5a6f23b7e
LOGAN LEE <dlfjgkssk1@naver.com>
2020-08-22 22:49:10 +0100

    attach VirtuaList to Ranking contaienr

--- src/container/Ranking/index.tsx
+++ src/container/Ranking/index.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { VirtualList } from 'components/VirtualList';
+
+import { state } from 'container/System/Slice';
+
+import { Wrapper } from './Wrapper';
+import { RootState } from 'store';
 
 interface Props {}
 
-export const Ranking = ({}: Props) => <div>{`stastica`}</div>;
+export const Ranking = ({}: Props) => {
+	const posts = useSelector((state: RootState) => state.system.posts);
+
+	// console.dir(posts);
+
+	return (
+		<Wrapper>
+			<VirtualList list={posts} showAuthor={true} showHit={true} showUploadDate={true} />
+		</Wrapper>
+	);
+};
