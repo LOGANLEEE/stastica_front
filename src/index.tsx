5573bfffa40fce8e092eaf929986783021f1fe81
LOGAN LEE <dlfjgkssk1@naver.com>
2020-08-22 15:09:00 +0100

    moving lcoation of App

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import { App } from './app';
 import { store } from './store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+
+import './index.css';
 
 ReactDOM.render(
 	<React.StrictMode>
