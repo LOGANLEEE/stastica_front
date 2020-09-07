7595dde5c55dbd9da7eae154f653b8357f3b692c
LOGAN LEE <dlfjgkssk1@naver.com>
2020-09-07 13:22:21 +0100

    take comp out from state to make it pure.

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import Button from '@material-ui/core/Button';
+import { Button, OverridableComponent, SvgIconTypeMap } from '@material-ui/core';
+import icons from '@material-ui/icons';
+import { Home, Receipt, AccountBalance, Poll } from '@material-ui/icons';
 
 import { useHistory } from 'react-router';
 
 import { Menu } from 'container/Ui/Slice';
+
+import { Esential } from 'app';
 
 const Wrapper = styled.div`
 	text-align: center;
@@ -18,18 +22,35 @@ interface Props {
 	menus: Menu[];
 }
 
-export const Navigation = ({ menus }: Props) => {
-	const history = useHistory();
+const startIconHandler = (startIcon: string): OverridableComponent<SvgIconTypeMap<{}, 'svg'>> => {
+	let result: OverridableComponent<SvgIconTypeMap<{}, 'svg'>> = <Home />;
+	switch (startIcon) {
+		case 'Hone':
+			result = <Home />;
+		case 'Receipt':
+			result = <Receipt />;
+		case 'AccountBalance':
+			result = <AccountBalance />;
+		case 'Poll':
+			result = <Poll />;
+	}
+	return result;
+};
 
+// console.log(icons['Home']);
+
+export const Navigation = ({ menus }: Props & Esential) => {
+	const history = useHistory();
 	return (
 		<Wrapper>
-			{menus.map(({ name, text, color, startIcon }: Menu) => (
+			{menus.map(({ name, text, color, startIcon }: Menu, idx) => (
 				<Button
 					variant='contained'
 					color={color}
 					className='btn'
-					startIcon={startIcon}
-					key={`comp > Navigation > ${name} > ${Math.random()}`}
+					startIcon={startIconHandler(startIcon)}
+					// startIcon={icons[startIcon]}
+					key={`comp > Navigation > ${name} > ${idx}`}
 					onClick={() => history.push(text)}>
 					{name}
 				</Button>
@@ -50,5 +71,5 @@ Navigation.propTypes = {
 };
 
 Navigation.defaultProps = {
-	menus: [{ text: '', name: '', color: '', startIcon: null }],
+	menus: [{ text: '', name: '', color: '', startIcon: <Home /> }],
 };
--- src/container/Ui/Slice.tsx
+++ src/container/Ui/Slice.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { AppThunk, RootState } from 'store';
 
-import { OverridableComponent } from '@material-ui/core/OverridableComponent';
-import { SvgIconTypeMap } from '@material-ui/core';
-
-import { Home as HomeIcon, Receipt, AccountBalance, Poll } from '@material-ui/icons';
-
 import { router } from 'router';
 
 const { bigData, exchangeRate, home, news, ranking } = router;
@@ -15,44 +10,55 @@ export interface Menu {
 	name: string;
 	text: string;
 	color: string | any;
-	startIcon: OverridableComponent<SvgIconTypeMap<{}, 'svg'>> | any;
+	startIcon: string;
 }
 
-export interface uiSliceState {
+export interface ViewOption {
+	viewAuthor: boolean;
+	viewHitCount: boolean;
+	viewDate: boolean;
+}
+export interface UiState {
 	isDark: boolean;
 	menus: Menu[];
+	viewOption: ViewOption;
 }
 
-interface EnumServiceGetOrderBy {
-	[index: number]: { id: number; label: string; key: any };
-}
+// interface EnumServiceGetOrderBy {
+// 	[index: number]: { id: number; label: string; key: any };
+// }
 
-const initialState: uiSliceState = {
+const initialState: UiState = {
 	isDark: false,
+	viewOption: {
+		viewAuthor: true,
+		viewDate: true,
+		viewHitCount: true,
+	},
 	menus: [
 		{
 			name: 'Main',
 			text: home,
 			color: 'secondary',
-			startIcon: <HomeIcon />,
+			startIcon: 'Home',
 		},
 		{
 			name: 'Ranking',
 			text: ranking,
 			color: 'secondary',
-			startIcon: <AccountBalance />,
+			startIcon: 'AccountBalance',
 		},
 		{
 			name: 'News',
 			text: news,
 			color: 'default',
-			startIcon: <Receipt />,
+			startIcon: 'Receipt',
 		},
 		{
 			name: 'BigData',
 			text: bigData,
 			color: 'primary',
-			startIcon: <Poll />,
+			startIcon: 'Poll',
 		},
 	],
 };
@@ -61,15 +67,19 @@ export const uiSlice = createSlice({
 	name: 'ui',
 	initialState,
 	reducers: {
-		THEME_HANDLER: (state) => {
+		THEME_HANDLER: (state: UiState) => {
 			state.isDark = !state.isDark;
+		},
+		SET_VIEW_OPTION: (state: UiState, action: PayloadAction<ViewOption>) => {
+			state.viewOption = action.payload;
 		},
 	},
 });
 
-export const { THEME_HANDLER } = uiSlice.actions;
+export const { THEME_HANDLER, SET_VIEW_OPTION } = uiSlice.actions;
 
 export const selectIsDark = (state: RootState) => state.ui.isDark;
 export const selectMenus = (state: RootState) => state.ui.menus;
+export const selectViewOption = (state: RootState) => state.ui.viewOption;
 
 export default uiSlice.reducer;
