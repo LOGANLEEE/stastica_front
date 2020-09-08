7cd88f2b840251544cfb5826e125e5b2c7756153
LOGAN LEE <dlfjgkssk1@naver.com>
2020-09-08 23:41:38 +0100

    correcting width

--- src/app/Wrapper.ts
+++ src/app/Wrapper.ts
@@ -3,29 +3,34 @@ import { Style } from './index';
 
 export const Wrapper = styled.div<Style>`
 	background-color: #1b1b1b;
+
 	color: white;
+	width: 100vw;
 
 	.head {
+		width: 100%;
 		height: ${(props) => props.head.container_height}vh;
 		padding-top: 0.1%;
-}
 	}
 
 	.body {
+		width: 100%;
 		height: ${(props) => props.body.container_height}vh;
+
 		.left {
-			width: 15vw;
+			width: 15%;
 		}
 		.middle {
-			width: 70vw;
+			width: 70%;
 		}
 
 		.right {
-			width: 15vw;
+			width: 15%;
 		}
 	}
 
 	.foot {
+		width: 100%;
 		height: ${(props) => props.foot.container_height}vh;
 	}
 `;
