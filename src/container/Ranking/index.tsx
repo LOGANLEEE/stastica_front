96554cff9423eed70685365eef58cccef4a62859
LOGAN LEE <dlfjgkssk1@naver.com>
2020-09-08 16:22:16 +0100

    pass viewOptions from container to comp

--- src/container/Ranking/index.tsx
+++ src/container/Ranking/index.tsx
@@ -4,16 +4,18 @@ import { useSelector } from 'react-redux';
 import { VirtualList } from 'components/VirtualList';
 
 import { selectPosts } from 'container/System/Slice';
+import { selectViewOption } from 'container/Ui/Slice';
 
 import { Wrapper } from './Wrapper';
 import { Esential } from 'app';
 
 export const Ranking = ({ isDark }: Esential) => {
 	const posts = useSelector(selectPosts);
+	const { viewAuthor, viewDate, viewHitCount } = useSelector(selectViewOption);
 
 	return (
 		<Wrapper>
-			<VirtualList list={posts} showAuthor={true} showHit={true} showUploadDate={true} />
+			<VirtualList list={posts} viewAuthor={viewAuthor} viewDate={viewDate} viewHitCount={viewHitCount} />
 		</Wrapper>
 	);
 };
