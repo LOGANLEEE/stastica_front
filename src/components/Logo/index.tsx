da1211327914c0a1e13a4a132dd142718255ca25
LOGAN LEE <dlfjgkssk1@naver.com>
2020-09-11 01:04:08 +0100

    add logo

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Wrapper } from './Wrapper';
+
+import { Esential } from 'app';
+
+interface Props {}
+
+export const Logo = ({}: Props & Esential) => <Wrapper>Stastica</Wrapper>;
