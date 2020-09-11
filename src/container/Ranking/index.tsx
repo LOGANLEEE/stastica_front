5f31e6cced8467e1069bcd9ebe680fcc3831e893
LOGAN LEE <dlfjgkssk1@naver.com>
2020-09-11 01:03:12 +0100

    linting

--- src/container/Ranking/index.tsx
+++ src/container/Ranking/index.tsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux';
 
 import { VirtualList } from 'components/VirtualList';
 
-import { selectPosts } from 'container/System/Slice';
+import { selectPosts, selectIsPostLoaded } from 'container/System/Slice';
 import { selectViewOption } from 'container/Ui/Slice';
 
 import { Wrapper } from './Wrapper';
@@ -11,11 +11,18 @@ import { Esential } from 'app';
 
 export const Ranking = ({ isDark }: Esential) => {
 	const posts = useSelector(selectPosts);
+	const isPostLoaded = useSelector(selectIsPostLoaded);
 	const { viewAuthor, viewDate, viewHitCount } = useSelector(selectViewOption);
 
 	return (
 		<Wrapper>
-			<VirtualList list={posts} viewAuthor={viewAuthor} viewDate={viewDate} viewHitCount={viewHitCount} />
+			<VirtualList
+				isPostLoaded={isPostLoaded}
+				list={posts}
+				viewAuthor={viewAuthor}
+				viewDate={viewDate}
+				viewHitCount={viewHitCount}
+			/>
 		</Wrapper>
 	);
 };
