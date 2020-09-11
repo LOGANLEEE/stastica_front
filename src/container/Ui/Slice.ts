7a2ec646597ed0ae6c87080f116e56b9bcba4bb3
LOGAN LEE <dlfjgkssk1@naver.com>
2020-09-11 01:03:25 +0100

    add new state and action and select

--- src/container/Ui/Slice.ts
+++ src/container/Ui/Slice.ts
@@ -1,16 +1,22 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { AppThunk, RootState } from 'store';
 
-import { router } from 'router';
-
-const { bigData, exchangeRate, home, news, ranking } = router;
-
 export interface Menu {
 	name: string;
 	text: string;
 	color: string | any;
 	startIcon: string;
 }
+
+export const pageSet = {
+	home: 'home',
+	ranking: 'ranking',
+	exchangeRate: 'exchangeRate',
+	bigData: 'bigData',
+	news: 'news',
+};
+
+const { bigData, exchangeRate, home, news, ranking } = pageSet;
 
 export interface ViewOption {
 	viewAuthor: boolean;
@@ -20,6 +26,7 @@ export interface ViewOption {
 export interface UiState {
 	isDark: boolean;
 	menus: Menu[];
+	currentPage: string;
 	viewOption: ViewOption;
 }
 
@@ -30,27 +37,28 @@ const initialState: UiState = {
 		viewDate: true,
 		viewHitCount: true,
 	},
+	currentPage: home,
 	menus: [
 		{
-			name: 'Main',
+			name: home,
 			text: home,
 			color: 'secondary',
 			startIcon: 'Home',
 		},
 		{
-			name: 'Ranking',
+			name: ranking,
 			text: ranking,
 			color: 'secondary',
 			startIcon: 'AccountBalance',
 		},
 		{
-			name: 'News',
+			name: news,
 			text: news,
 			color: 'default',
 			startIcon: 'Receipt',
 		},
 		{
-			name: 'BigData',
+			name: bigData,
 			text: bigData,
 			color: 'primary',
 			startIcon: 'Poll',
@@ -68,13 +76,17 @@ export const uiSlice = createSlice({
 		SET_VIEW_OPTION: (state: UiState, action: PayloadAction<ViewOption>) => {
 			state.viewOption = action.payload;
 		},
+		SET_CURRENT_PAGE: (state: UiState, action: PayloadAction<string>) => {
+			state.currentPage = action.payload;
+		},
 	},
 });
 
-export const { THEME_HANDLER, SET_VIEW_OPTION } = uiSlice.actions;
+export const { THEME_HANDLER, SET_VIEW_OPTION, SET_CURRENT_PAGE } = uiSlice.actions;
 
 export const selectIsDark = (state: RootState) => state.ui.isDark;
 export const selectMenus = (state: RootState) => state.ui.menus;
 export const selectViewOption = (state: RootState) => state.ui.viewOption;
+export const selectCurrentPage = (state: RootState) => state.ui.currentPage;
 
 export default uiSlice.reducer;
