9b9c084111e2195402d4602a273d588d9a9d9209
LOGAN LEE <dlfjgkssk1@naver.com>
2020-09-14 23:54:45 +0100

    add styles

--- src/app/Wrapper.ts
+++ src/app/Wrapper.ts
@@ -2,73 +2,59 @@ import styled from 'styled-components';
 import { Style } from './index';
 
 export const Wrapper = styled.div<Style>`
-
-:root
-
-	background-color: #1b1b1b;
-	font-size: 16px;
-
-	 /* @media only screen and (max-width: 600px) {
+	/* @media only screen and (max-width: 600px) {
 		font-size: 12px;
 		} */
 
-	color: white;
+	font-size: 16px;
+	background-color: #1b1b1b;
+	color: #e6e6e6;
 	width: 100vw;
+	height: 100vh;
 
 	.head {
-		height: ${(props) => props.head.container_height}vh;
-		padding-top: 0.2%;
+		background-color: #2b2b2b;
+		height: ${(props) => props.head.container_height}%;
+		/* max-height: ${(props) => props.head.container_height}%; */
+		/* max-height: 10%; */
+		/* min-height: 5%; */
+		padding: 0.4%;
+
+		.item {
+			height: 100%;
+		}
+
+		.head_item_0 {
+			width: 7%;
+		}
 
 		.head_item_1 {
-			width:50%;
-
+			width: 20%;
 		}
+
+		.brick {
+			@media only screen and (max-width: 600px) {
+				width: 33%;
+			}
+			@media only screen and (min-width: 601px) {
+				width: 63%;
+			}
+		}
+
 		.head_item_2 {
-			width:5%;
+			width: 5%;
 		}
 		.head_item_3 {
-			width:5%;
-
+			width: 5%;
 		}
 	}
 
 	.body {
-		height: ${(props) => props.body.container_height}vh;
-
-		.left {
-			width: 15%;
-		}
-		.middle {
-			width: 70%;
-		}
-
-		.right {
-			width: 15%;
-
-			.item{
-				width:100%;
-			}
-		}
+		height: ${(props) => props.body.container_height}%;
 	}
 
-	/* @media only screen and (max-width: 600px) {
-		.body {
-			height: ${(props) => props.body.container_height}vh;
-
-			.left {
-				width: 10%;
-			}
-			.middle {
-				width: 10%;
-			}
-
-			.right {
-				width: 100%;
-			}
-		}
-	} */
-
 	.foot {
-		height: ${(props) => props.foot.container_height}vh;
+		background-color: #2b2b2b;
+		height: ${(props) => props.foot.container_height}%;
 	}
 `;
