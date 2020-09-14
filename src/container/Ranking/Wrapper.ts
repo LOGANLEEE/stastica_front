a226de1f0f6368d6d1e6276c42edb854e2c7d6f2
LOGAN LEE <dlfjgkssk1@naver.com>
2020-09-14 23:46:06 +0100

    move body logic from app.tsx to contianer

--- src/container/Ranking/Wrapper.ts
+++ src/container/Ranking/Wrapper.ts
@@ -2,4 +2,26 @@ import styled from 'styled-components';
 
 export const Wrapper = styled.div`
 	height: 100%;
+
+	.container {
+		height: 100%;
+
+		.left {
+			background-color: #2b2b2b;
+			width: 10%;
+		}
+		.middle {
+			background-color: #1b1b1b;
+			width: 80%;
+		}
+
+		.right {
+			background-color: #2b2b2b;
+			width: 10%;
+
+			.item {
+				width: 100%;
+			}
+		}
+	}
 `;
--- src/container/Ranking/index.tsx
+++ src/container/Ranking/index.tsx
@@ -1,28 +1,51 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { Grid } from '@material-ui/core';
 
+import { selectViewOption, ViewOption } from 'container/Ui/Slice';
+
+import { selectPosts, selectIsPostLoaded, Post } from 'container/System/Slice';
+
+import { ListOption } from 'components/ListOption';
 import { VirtualList } from 'components/VirtualList';
-
-import { selectPosts, selectIsPostLoaded } from 'container/System/Slice';
-import { selectViewOption } from 'container/Ui/Slice';
+import { PostOpener } from 'components/PostOpener';
 
 import { Wrapper } from './Wrapper';
-import { Esential } from 'app';
+import { Essential } from 'app';
 
-export const Ranking = ({ isDark }: Esential) => {
-	const posts = useSelector(selectPosts);
-	const isPostLoaded = useSelector(selectIsPostLoaded);
-	const { viewAuthor, viewDate, viewHitCount } = useSelector(selectViewOption);
+interface Props {}
+
+export const Ranking = (props: Props & Essential) => {
+	const posts: Post[] = useSelector(selectPosts);
+	const isPostLoaded: boolean = useSelector(selectIsPostLoaded);
+	const { viewAuthor, viewDate, viewHitCount, viewFrom }: ViewOption = useSelector(selectViewOption);
 
 	return (
 		<Wrapper>
-			<VirtualList
-				isPostLoaded={isPostLoaded}
-				list={posts}
-				viewAuthor={viewAuthor}
-				viewDate={viewDate}
-				viewHitCount={viewHitCount}
-			/>
+			<Grid className='container' container direction='row' justify='space-around' alignItems='stretch'>
+				<Grid container item className='left'>
+					<Grid item className='item'>
+						<PostOpener {...props} isPostLoaded={isPostLoaded} posts={posts} />
+					</Grid>
+				</Grid>
+
+				<Grid item className='middle'>
+					<VirtualList
+						isPostLoaded={isPostLoaded}
+						posts={posts}
+						viewAuthor={viewAuthor}
+						viewDate={viewDate}
+						viewHitCount={viewHitCount}
+						viewFrom={viewFrom}
+					/>
+				</Grid>
+
+				<Grid container item className='right'>
+					<Grid item className='item'>
+						<ListOption {...props} />
+					</Grid>
+				</Grid>
+			</Grid>
 		</Wrapper>
 	);
 };
