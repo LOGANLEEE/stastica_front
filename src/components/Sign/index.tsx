265d53c765cd90eabd2529b22f2aeaa2b30f5b94
LOGAN LEE <dlfjgkssk1@naver.com>
2020-09-15 00:04:42 +0100

    add sign Comp

--- src/components/Sign/index.tsx
+++ src/components/Sign/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Fingerprint } from '@material-ui/icons';
 
-import { Esential } from 'app';
+import { Essential } from 'app';
 
 import { Wrapper } from './Wrapper';
 
 interface Props {}
 
-export const Sign = ({ isDark }: Props & Esential) => (
+export const Sign = ({ isDark }: Props & Essential) => (
 	<Wrapper>
 		<Fingerprint />
 	</Wrapper>
