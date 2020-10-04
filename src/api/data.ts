fcb74bb11f55aca5985317a9af43ec3883ec4a9e
LOGAN LEE <dlfjgkssk1@naver.com>
2020-10-05 06:01:15 +0900

    mod api name

--- src/api/data.ts
+++ src/api/data.ts
@@ -2,12 +2,12 @@ import { xios } from 'api';
 
 const { dir, log } = console;
 
-export const GET_ALL_POSTS = (url: string) => {
+export const GET_DATA = (url: string) => {
 	return xios.post(url).then(({ data, status }) => {
 		return { data, status };
 	});
 };
 
 export const data = {
-	GET_ALL_POSTS,
+	GET_DATA: GET_DATA,
 };
